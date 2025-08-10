24dea848e8088096fe81606de3abf32240c19894
Mlink <indiegomail@gmail.com>
2025-08-10 17:01:34 +0300

    page profile correction

--- app.py
+++ app.py
@@ -95,5 +95,4 @@ def profile():
 
 
 if __name__ == "__main__":
-    # не вызываем db.create_all() — мы будем использовать миграции
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
